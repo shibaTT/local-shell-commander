54a8feda338ea586952f74759b01f168e1971048
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-02-13 22:38:04 +0900

    feat: パルワールドのサーバーを起動するコマンドに変更

--- src/app/api/commands/route.js
+++ src/app/api/commands/route.js
@@ -1,14 +1,17 @@
 import { execa } from "execa";
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    const temp = await execa("vcgencmd", ["measure_temp"]).catch(() => null);
-    const volts = await execa("vcgencmd", ["measure_volts"]).catch(() => null);
+export async function GET(request, response) {
+    // const temp = await execa("vcgencmd", ["measure_temp"]).catch(() => null);
+    // const volts = await execa("vcgencmd", ["measure_volts"]).catch(() => null);
 
-    const result = {
-        temp: temp ? temp.stdout : "",
-        volts: volts ? volts.stdout : "",
-    };
+    // const result = {
+    //     temp: temp ? temp.stdout : "",
+    //     volts: volts ? volts.stdout : "",
+    // };
 
-    return NextResponse.json(result, { status: 200 });
+    // return NextResponse.json(result, { status: 200 });
+
+    await execa("startpal");
+    return response.status(200);
 }
