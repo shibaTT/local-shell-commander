049a76f6601974c79f24e4688ee992fb761cc19d
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-02-13 22:00:39 +0900

    feat: APIテスト

---
+++ src/app/api/commands/route.js
@@ -0,0 +1,14 @@
+import { execa } from "execa";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const temp = await execa("vcgencmd", ["measure_temp"]).catch(() => null);
+    const volts = await execa("vcgencmd", ["measure_volts"]).catch(() => null);
+
+    const result = {
+        temp: temp ? temp.stdout : "",
+        volts: volts ? volts.stdout : "",
+    };
+
+    return NextResponse.json(result, { status: 200 });
+}
