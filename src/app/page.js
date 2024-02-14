0f8861565e007ea1456cf92bf0da2db70fb38ec8
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-02-14 22:11:58 +0900

    feat: ラズペリーバイの状況を見れるようにした

--- src/app/page.js
+++ src/app/page.js
@@ -1,137 +1,77 @@
 "use client";
-import Image from "next/image";
 import { HomeCards } from "@/components/parts/HomeCards";
 import { Content } from "@/components/parts/Content";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const handleClickPalworldStatus = async () => {
-        await fetch("/api/commands/palworld")
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch(() => console.error("パルワールドサーバー取得エラー"));
-    };
+    const [raspTemp, setRaspTemp] = useState("0'C");
+    const [raspVolts, setRaspVolts] = useState("0V");
+    const [raspClock, setRaspClock] = useState("0Mhz");
 
-    const handleClickPalworld = async () => {
-        const params = {
-            method: "POST",
-            headers: "application/json",
-            body: {
-                type: "start",
-            },
-        };
-        await fetch("/api/commands/palworld", params)
-            .then((response) => response.json())
-            .then((data) => console.log(data))
-            .catch(() => console.error("パルワールドサーバー開始/終了エラー"));
-    };
-
-    const handleClickRaspberryStatus = async () => {
-        await fetch("api/commands/raspberry")
-            .then((res) => res.json())
-            .then((data) => console.log(data))
-            .catch(() => console.error("ラズパイ情報取得エラー"));
-    };
-
-    // return (
-    //     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    //         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-    //             <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-    //                 Get started by editing&nbsp;
-    //                 <code className="font-mono font-bold">src/app/page.js</code>
-    //             </p>
-    //             <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-    //                 <a
-    //                     className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-    //                     href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //                     target="_blank"
-    //                     rel="noopener noreferrer"
-    //                 >
-    //                     By{" "}
-    //                     <Image
-    //                         src="/vercel.svg"
-    //                         alt="Vercel Logo"
-    //                         className="dark:invert"
-    //                         width={100}
-    //                         height={24}
-    //                         priority
-    //                     />
-    //                 </a>
-    //             </div>
-    //         </div>
-
-    //         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-    //             <Image
-    //                 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-    //                 src="/next.svg"
-    //                 alt="Next.js Logo"
-    //                 width={180}
-    //                 height={37}
-    //                 priority
-    //             />
-    //         </div>
-
-    //         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-    //             <div
-    //                 onClick={handleClickPalworldStatus}
-    //                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //             >
-    //                 <h2 className={`mb-3 text-2xl font-semibold`}>
-    //                     Palworld(GET)
-    //                 </h2>
-    //                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //                     Directry access to Palworld Local Server API.
-    //                 </p>
-    //             </div>
-
-    //             <div
-    //                 onClick={handleClickPalworld}
-    //                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-    //             >
-    //                 <h2 className={`mb-3 text-2xl font-semibold`}>
-    //                     Palworld(POST)
-    //                 </h2>
-    //                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //                     Fetch palworld local server. (Server will start)
-    //                 </p>
-    //             </div>
-
-    //             <div
-    //                 onClick={handleClickRaspberryStatus}
-    //                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //             >
-    //                 <h2 className={`mb-3 text-2xl font-semibold`}>
-    //                     Raspberry Pi Status
-    //                 </h2>
-    //                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-    //                     Get Raspberry Pi Status.(check your console log)
-    //                 </p>
-    //             </div>
-
-    //             <a
-    //                 href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-    //                 target="_blank"
-    //                 rel="noopener noreferrer"
-    //             >
-    //                 <h2 className={`mb-3 text-2xl font-semibold`}>
-    //                     Deploy{" "}
-    //                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-    //                         -&gt;
-    //                     </span>
-    //                 </h2>
-    //                 <p
-    //                     className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}
-    //                 >
-    //                     Instantly deploy your Next.js site to a shareable URL
-    //                     with Vercel.
-    //                 </p>
-    //             </a>
-    //         </div>
-    //     </main>
-    // );
-
+    useEffect(() => {});
     return (
         <Content className="flex flex-col flex-1 w-0 overflow-hidden h-screen">
+            <div className="relative items-center w-full px-3 py-6">
+                <div className="w-full mx-auto p-6 bg-white shadow-xl rounded-xl">
+                    <p className="text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl whitespace-pre-wrap">
+                        ラズペリーバイの状況
+                        <span className="ml-4 text-xs font-semibold tracking-widest text-blue-600 uppercase">
+                            Status
+                        </span>
+                    </p>
+
+                    <section className="grid max-w-lg mt-5 gap-5 mx-auto divide-x lg:grid-cols-3 lg:max-w-none">
+                        <div className="relative items-center w-full px-3 py-6 max-w-7xl">
+                            <div className="grid grid-cols-1">
+                                <div className="w-full max-w-lg mx-auto my-4 bg-white">
+                                    <div className="p-6 lg:text-center">
+                                        <h4 className="my-4 text-xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl whitespace-pre-wrap">
+                                            温度
+                                        </h4>
+
+                                        <p className="mt-3 text-base leading-relaxed text-gray-500 whitespace-pre-wrap">
+                                            {raspTemp}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative items-center w-full px-3 py-6 max-w-7xl">
+                            <div className="grid grid-cols-1">
+                                <div className="w-full max-w-lg mx-auto my-4 bg-white">
+                                    <div className="p-6 lg:text-center">
+                                        <h4 className="my-4 text-xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl whitespace-pre-wrap">
+                                            電圧
+                                        </h4>
+
+                                        <p className="mt-3 text-base leading-relaxed text-gray-500 whitespace-pre-wrap">
+                                            {raspVolts} × 5A =<br />
+                                            {Number.parseFloat(raspVolts) * 5 + "W"}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="relative items-center w-full px-3 py-6 max-w-7xl">
+                            <div className="grid grid-cols-1">
+                                <div className="w-full max-w-lg mx-auto my-4 bg-white">
+                                    <div className="p-6 lg:text-center">
+                                        <h4 className="my-4 text-xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl whitespace-pre-wrap">
+                                            クロック数
+                                        </h4>
+
+                                        <p className="mt-3 text-base leading-relaxed text-gray-500 whitespace-pre-wrap">
+                                            {raspClock}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
             <HomeCards />
         </Content>
     );
