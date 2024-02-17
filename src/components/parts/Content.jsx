76986fadf6fb9f6af6f4e916b4ac89c9c85310e3
ShibaTTTTTTTTT <39818834+shibaTT@users.noreply.github.com>
2024-02-17 19:19:11 +0900

    update: refactoring

--- src/components/parts/Content.jsx
+++ src/components/parts/Content.jsx
@@ -1,17 +1,13 @@
 export function Content(props) {
     return (
-        <main className="flex flex-col flex-1 w-0 overflow-hidden h-screen bg-slate-50">
-            <main className="relative flex-1 overflow-y-auto focus:outline-none">
-                <div className="py-6">
-                    <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
-                        <div className="py-4">
-                            <div className="rounded-lg h-96">
-                                {props.children}
-                            </div>
-                        </div>
+        <main className="flex flex-col flex-1 w-0 overflow-hidden overflow-y-auto h-screen bg-slate-50 focus:outline-none">
+            <div className="py-6">
+                <div className="px-4 mx-auto max-w-7xl sm:px-6 md:px-8">
+                    <div className="py-4">
+                        <div className="rounded-lg h-96">{props.children}</div>
                     </div>
                 </div>
-            </main>
+            </div>
         </main>
     );
 }
